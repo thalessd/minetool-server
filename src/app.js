1906a1dabd8703187164b05fb296c1de9e1b1706
Thales Carvalho <thalessd@live.com>
2020-03-27 15:42:44 -0300

    Socket io implementado

--- src/app.js
+++ src/app.js
@@ -1,21 +1,15 @@
 import "moment/locale/pt-br";
 import env from "dotenv";
+import socketIO from "socket.io";
 import { MinecraftServer } from "./services/minecraft-server";
 import { Constants } from "./constants/constants";
-import readline from "readline";
+import { Controller } from "./controllers/controller";
 
 env.config();
 
-function blueLog(str) {
-  console.log(`\u001b[36m${str}\u001b[0m`);
-}
-
-console.bLog = blueLog;
-
 async function app() {
   try {
-    readline.emitKeypressEvents(process.stdin);
-    process.stdin.setRawMode(true);
+    const io = socketIO(80);
 
     const minecraftServer = new MinecraftServer(
       Constants.MINECRAFT_SERVER_PATH()
@@ -23,76 +17,26 @@ async function app() {
 
     minecraftServer.logs = true;
 
-    // Test Events
-    minecraftServer.onServerStart(() => {
-      console.log("O Servidor abriu");
-    });
+    const controller = new Controller(io, minecraftServer);
 
-    minecraftServer.onServerDone(() => {
-      console.log("O Servidor está rodando");
-    });
+    // IO Emitters
+    controller.emitServerRun("server_running");
+    controller.emitServerDone("server_is_done");
+    controller.emitServerStop("server_stopped");
+    controller.emitServerError("server_errored");
+    controller.emitUserLogged("user_logged");
+    controller.emitUserLoggedOut("user_logged_out");
+    controller.emitUserMessage("user_message");
+    // controller.emitUserMessageWithCode("user_message_#", "note");
 
-    minecraftServer.onUserLogged((data) => {
-      console.log("Dados do Usuário Logado", data);
-    });
-
-    minecraftServer.onUserLoggedOut((data) => {
-      console.log("Dados do Usuário Deslogado", data);
-    });
-
-    minecraftServer.onMessageWithCode("nota", (data) => {
-      console.log("Mensagem com código vinda do jogo", data);
-    });
-
-    minecraftServer.onMessage((data) => {
-      console.log("Mensagem vinda do jogo", data);
-    });
-
-    minecraftServer.onServerKill(() => {
-      console.log("O Servidor foi morto");
-    });
-
-    minecraftServer.onServerError(() => {
-      console.log("O Servidor está com erro.");
-    });
-
-    minecraftServer.run();
-
-    // Test Interface
-    process.stdin.on("keypress", (str, key) => {
-      if (key.ctrl && key.name === "c") {
-        return process.exit();
-      }
-
-      switch (key.name) {
-        case "1":
-          console.bLog("Iniciou o Server");
-          minecraftServer.run();
-          return;
-        case "2":
-          console.bLog("Reiniciou o Server");
-          minecraftServer.restart();
-          return;
-        case "3":
-          console.bLog("Matou o Server");
-          minecraftServer.kill();
-          process.exit();
-          return;
-        case "4":
-          console.bLog("Enviar Mensagem");
-          minecraftServer.sendSay("Teste de Say em jogo");
-          return;
-        case "5":
-          console.bLog("Mostrar Lista de Usuários Online");
-          console.log(minecraftServer.getListOnlineUser());
-          return;
-        case "6":
-          console.bLog("Kickar usuário do Server");
-          minecraftServer.sendKick("thalessd", "Usuário Insano");
-          return;
-        default:
-          return;
-      }
+    // IO Listners
+    io.on("connection", (socket) => {
+      controller.onServerRun(socket, "server_run");
+      controller.onServerStop(socket, "server_stop");
+      controller.onServerStatus(socket, "server_status", "server_status_data");
+      controller.onServerRestart(socket, "server_restart");
+      controller.onSay(socket, "say");
+      controller.onKick(socket, "kick");
     });
   } catch (e) {
     throw e;
---
+++ src/controllers/controller.js
@@ -0,0 +1,95 @@
+export class Controller {
+  constructor(io, minecraftServer) {
+    this.io = io;
+    this.minecraftServer = minecraftServer;
+  }
+
+  onSay(socket, str) {
+    socket.on(str, (message, flags = "") => {
+      this.minecraftServer.sendSay(message, flags);
+    });
+  }
+
+  onKick(socket, str) {
+    socket.on(str, (message, reason = "") => {
+      this.minecraftServer.sendKick(message, reason);
+    });
+  }
+
+  onServerStatus(socket, onStr, emitStr) {
+    socket.on(onStr, () => {
+      const serverStatus = {
+        serverOnline: this.minecraftServer.isOnline,
+        playersOnline: this.minecraftServer.getListOnlineUser(),
+      };
+
+      socket.emit(emitStr, serverStatus);
+    });
+  }
+
+  onServerRun(socket, str) {
+    socket.on(str, () => {
+      this.minecraftServer.run();
+    });
+  }
+
+  onServerStop(socket, str) {
+    socket.on(str, () => {
+      this.minecraftServer.kill();
+    });
+  }
+
+  onServerRestart(socket, str) {
+    socket.on(str, () => {
+      this.minecraftServer.restart();
+    });
+  }
+
+  emitServerRun(str) {
+    this.minecraftServer.onServerStart(() => {
+      this.io.emit(str);
+    });
+  }
+
+  emitServerDone(str) {
+    this.minecraftServer.onServerDone(() => {
+      this.io.emit(str);
+    });
+  }
+
+  emitServerStop(str) {
+    this.minecraftServer.onServerKill(() => {
+      this.io.emit(str);
+    });
+  }
+
+  emitServerError(str) {
+    this.minecraftServer.onServerError(() => {
+      this.io.emit(str);
+    });
+  }
+
+  emitUserLogged(str) {
+    this.minecraftServer.onUserLogged((data) => {
+      this.io.emit(str, data);
+    });
+  }
+
+  emitUserLoggedOut(str) {
+    this.minecraftServer.onUserLoggedOut((data) => {
+      this.io.emit(str, data);
+    });
+  }
+
+  emitUserMessage(str) {
+    this.minecraftServer.onMessage((data) => {
+      this.io.emit(str, data);
+    });
+  }
+
+  emitUserMessageWithCode(str, code) {
+    this.minecraftServer.onMessageWithCode(code, (data) => {
+      this.io.emit(str + code, data);
+    });
+  }
+}
